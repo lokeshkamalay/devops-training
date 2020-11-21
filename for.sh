4158960165f054c40e42bb02f7c8878742b58f07
Lokesh Kamalay <lokesh.mydilse@gmail.com>
2020-11-21 08:47:59 -0600

    Modified the for.sh as per reviewer's comment

--- for.sh
+++ for.sh
@@ -1,7 +1,8 @@
 #!/bi/sh
 clear
-read -p "Enter a number: " num
-
+#read -p "Enter a number: " num
+echo "Taking a random number"
+num=10
 for((i=0;i<=num;i=i+1)); do
 	echo "I'm in iteration: $i"
 done
