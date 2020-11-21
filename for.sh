6087dda8c2f39eaae27347fced8e08893caee6c8
Lokesh Kamalay <lokesh.mydilse@gmail.com>
2020-11-21 09:00:53 -0600

    updated for loop

--- for.sh
+++ for.sh
@@ -1,8 +1,14 @@
 #!/bi/sh
 clear
 #read -p "Enter a number: " num
-echo "Taking a random number"
+echo "Printing from 1 to 10"
 num=10
+#this is the for loop
 for((i=0;i<=num;i=i+1)); do
+	echo "I'm in iteration: $i"
+done
+
+#this is the while loop
+while [[ "$i" -le "$num" ]]; do
 	echo "I'm in iteration: $i"
 done
